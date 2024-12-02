88773ccfdd8734a61925696743c7b878cf0074e6
Nikita Ignatovich <nikita@ignatovich.me>
2024-12-03 00:36:54 +0300

    setup unhead

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from 'vue';
 
 import App from './app.vue';
-import pinia from './configurations/pinia';
-import router from './configurations/router';
+import pinia from './configurations/pinia.ts';
+import router from './configurations/router.ts';
 import i18n from '@/configurations/i18n.ts';
+import unhead from '@/configurations/unhead.ts';
 
 const app = createApp(App);
 
-app.use(pinia).use(router).use(i18n).mount('#app');
+app.use(pinia).use(router).use(i18n).use(unhead).mount('#app');
