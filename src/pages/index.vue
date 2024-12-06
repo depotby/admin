95d0419f94feefa732c7f887fd0cf6c312e979b5
Nikita Ignatovich <nikita@ignatovich.me>
2024-12-06 16:40:38 +0300

    dashboard page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+import { useHead } from '@unhead/vue';
+
+const { t } = useI18n();
+
+useHead(() => ({
+  title: t('pages.index.meta.title'),
+}));
+</script>
 
 <template>
-  <div>index</div>
+  <div>Dashboard</div>
 </template>
