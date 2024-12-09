8dc64da6dccdb20f201e9bc0422301e341206557
Nikita Ignatovich <nikita@ignatovich.me>
2024-12-09 03:34:15 +0300

    new icons

--- src/types/assets/icons.ts
+++ src/types/assets/icons.ts
@@ -1,7 +1,12 @@
 export type Icon =
+  | 'add-2-rounded'
   | 'category-rounded'
+  | 'chevron-left-rounded'
   | 'dashboard-rounded'
+  | 'delete-rounded'
   | 'enterprise'
+  | 'eye-tracking-rounded'
+  | 'ink-pen-rounded'
   | 'loading'
   | 'menu-rounded'
   | 'supervised-user-circle';
