a5f2591826e7a3ed317bbcae722180bfd61454f2
Nikita Ignatovich <nikita@ignatovich.me>
2024-12-23 22:54:59 +0300

    add table to main tab of category

--- src/pages/categories/[id]/main.vue
+++ src/pages/categories/[id]/main.vue
@@ -6,8 +6,21 @@ const props = defineProps<{ category: ListCategory }>();
 
 <template>
   <div>
-    <pre>{{ props.category }}</pre>
+    <table :class="simple_table">
+      <tbody>
+        <tr>
+          <th>{{ $t('labels.name') }}</th>
+          <td>{{ props.category.name }}</td>
+        </tr>
+        <tr>
+          <th>{{ $t('labels.uri_name') }}</th>
+          <td>{{ props.category.uri_name }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<style module="simple_table" src="@/assets/styles/modules/simple-table.scss" />
 
 <style module lang="scss"></style>
