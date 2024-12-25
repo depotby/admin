0740038a213802f67f1a3655a505fd10887abdaa
Nikita Ignatovich <nikita@ignatovich.me>
2024-12-25 16:33:03 +0300

    remove `div` in main category tab

--- src/pages/categories/[id]/main.vue
+++ src/pages/categories/[id]/main.vue
@@ -5,22 +5,18 @@ const props = defineProps<{ category: ListCategory }>();
 </script>
 
 <template>
-  <div>
-    <table :class="simple_table">
-      <tbody>
-        <tr>
-          <th>{{ $t('labels.name') }}</th>
-          <td>{{ props.category.name }}</td>
-        </tr>
-        <tr>
-          <th>{{ $t('labels.uri_name') }}</th>
-          <td>{{ props.category.uri_name }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <table :class="simple_table">
+    <tbody>
+      <tr>
+        <th>{{ $t('labels.name') }}</th>
+        <td>{{ props.category.name }}</td>
+      </tr>
+      <tr>
+        <th>{{ $t('labels.uri_name') }}</th>
+        <td>{{ props.category.uri_name }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <style module="simple_table" src="@/assets/styles/modules/simple-table.scss" />
-
-<style module lang="scss"></style>
